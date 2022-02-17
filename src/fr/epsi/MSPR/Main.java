239b27a4f9acf5266f3e778df07cd1c355b18ec3
Mathys VENDE <mathys.vende@armor-group.com>
2022-02-17 09:08:27 +0100

    modif path image

--- src/fr/epsi/MSPR/Main.java
+++ src/fr/epsi/MSPR/Main.java
@@ -17,11 +17,11 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
-    private final static String StaffPath = "../ressources/staff.txt";
-    private final static String EquipementPath = "../ressources/liste.txt";
-    private final static String OutPutDirectory = "../ressources/";
-    private final static String AgentsTxtDirectory = "../ressources/ListAgents/";
-    private final static String AgentsPhotoDirectory = "../ressources/ListPhotosAgents/";
+    private final static String StaffPath = "ressources/staff.txt";
+    private final static String EquipementPath = "ressources/liste.txt";
+    private final static String OutPutDirectory = "ressources/";
+    private final static String AgentsTxtDirectory = "ressources/ListAgents/";
+    private final static String AgentsPhotoDirectory = "ressources/ListPhotosAgents/";
 
     public static void main(String[] args) throws IOException {
         ArrayList<String> ListStaffUncheck = ListStaff();
@@ -89,7 +89,7 @@ public class Main {
     public static void CreateHtmlFile(ArrayList<String> listAgents){
         for (String agent: listAgents) {
             String[] InfoSalarie = getContentTxtFile(AgentsTxtDirectory + agent + ".txt").toArray(new String[0]);
-            File file = new File("../ressources/template.html");
+            File file = new File("ressources/template.html");
             try {
                 Files.copy(file.toPath(),new File(OutPutDirectory+ agent+".html").toPath());
             } catch (IOException e) {
@@ -97,6 +97,7 @@ public class Main {
             }
             String nom = InfoSalarie[0]+" "+InfoSalarie[1];
             String mission = InfoSalarie[2];
+            String photo = "ListPhotosAgents"+agent+".jpg";
             String mousqueton;
             if(Arrays.toString(InfoSalarie).contains("mousqueton")) mousqueton = "checked"; else mousqueton="";
             String gant;
@@ -122,6 +123,7 @@ public class Main {
                 Path path = Paths.get(OutPutDirectory+ agent+".html");
                 Stream<String> lines = Files.lines(path);
                 List<String> replaced = lines.map(line -> line
+                        .replaceAll("\\$photo", photo)
                         .replaceAll("\\$nom", nom)
                         .replaceAll("\\$mission",mission)
                         .replaceAll("\\$mousqueton", mousqueton)
